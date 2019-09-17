ddfea5ca8ef4f485dee2ac847ec48f7f26a00e5a
Unknown <jialong0518@qq.com>
2019-09-17 22:53:59 +0800

    增加样式文件

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import './components.module.less';
+
+
 
 const NavBar = () =>(
-<div>
+<div className={'head_module'}>
     <div>
         <NavLink exact to='/'>Jsdemoa</NavLink> |&nbsp;
         <NavLink to='/Jsdemob'>Jsdemob</NavLink> |&nbsp;
